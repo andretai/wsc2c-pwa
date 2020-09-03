caa872c49c18ceee8b1d7ba3bbdf617f64319825
andretwx <andretai.wx@gmail.com>
2020-09-03 11:16:55 +0800

    Update footer.

--- src/fixed/Foot.js
+++ src/fixed/Foot.js
@@ -3,12 +3,13 @@ import React from 'react'
 export default function Foot() {
   return (
     <div>
-      <nav className="absolute bottom-0 w-full my-3 text-xs">
-        <div className="flex justify-center content-center">
+      <nav className="absolute bottom-0 w-full text-xs">
+        {/* <div className="flex justify-center content-center">
           <p>Created by</p>
           <a href="https://www.instagram.com/andretaiwx/" target="_blank" rel="noopener noreferrer"
             className="mx-1 text-indigo-500">Andre</a>
-        </div>
+        </div> */}
+        <p className="text-center">If there's any issue please contact <a className="text-indigo-500" href="https://www.instagram.com/andretaiwx/" target="_blank" rel="noopener noreferrer">me</a>.</p>
       </nav>
     </div>
   )
