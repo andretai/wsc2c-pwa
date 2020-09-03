d2ac109d682356b17bd6c467e10fb5021c372abd
andretwx <andretai.wx@gmail.com>
2020-09-03 11:30:45 +0800

    Update footer 5.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="relative min-h-screen">
+      <div className="min-h-screen">
         <Nav toggleShowPolicy={this.toggleShowPolicy} />
         { this.state.showPolicy ? <Policy toggleShowPolicy={this.toggleShowPolicy} /> : null }
         <Whatsapp />
